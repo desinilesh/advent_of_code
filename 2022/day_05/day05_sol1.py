61cfccbad199a85c6f56a9958d469ce3304690ea
desinilesh <45862755+desinilesh@users.noreply.github.com>
2022-12-28 19:53:14 -0800

    Day 5 solution

---
+++ 2022/day_05/day05_sol1.py
@@ -0,0 +1,36 @@
+import re
+
+from collections import deque
+
+INPUT = "input1.txt"
+with open(INPUT, "r") as f:
+    data = f.readlines()
+
+s = []
+stacks = deque()
+
+is_move_cmd = False
+for line in data:
+    if line.isspace():
+        is_move_cmd = True
+        num_stacks = int(s.pop()[-1])
+        for col in range(num_stacks):
+            crates = deque()
+            for row in range(len(s)):
+                if len(s[row]) <= col or s[row][col] == " ":
+                    continue
+                value = s[row][col]
+                crates.append(value)
+            stacks.append(crates)
+    elif not is_move_cmd:
+        s.append(line[1::4])
+    else:
+        num_items, src_col, dst_col = map(int, re.findall("\d+", line))
+        for i in range(num_items):
+            crate = stacks[src_col - 1].popleft()
+            stacks[dst_col - 1].appendleft(crate)
+
+print("Crates on top of each stack: ", end='')
+for stack in stacks:
+    print(stack[0], end='')
+print()
